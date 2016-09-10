89178b16203f72e30434ffd42dd2c84c78093200
Matt Wolf <mattwollf@gmail.com>
2016-09-10 17:06:52 -0400

    adding comments to test cases

--- ex1_test.rkt
+++ ex1_test.rkt
@@ -2,13 +2,15 @@
 (require rackunit
          "compilers_ex1.rkt")
 
+
+
 (test-begin
   (let ([tests (list
-              `(+ (read) -2)
-              `(+ 1 (+ (read) (+ (read) -2)))
-              `(+ 2 (+ (read) 2))
-              `(+ 2 (+ (read) (+ (+ (read) (- (read))) -4)))
-              `(+ (+ (+ 5 (read)) (- (read))) -4))])
+              `(+ (read) -2) ; test case: r1 is exp, r2 is int
+              `(+ 1 (+ (read) (+ (read) -2))) ; test case: switch exp and int, extract int and swap with read, sum r2 int with r1
+              `(+ 2 (+ (read) 2)) ; swap r1 and r2, sum r1 r2
+              `(+ 2 (+ (read) (+ (+ (read) (- (read))) -4))) ; subtract works still.
+              `(+ (+ (+ 5 (read)) (- (read))) -4))]) ; subtract read,, combine 5 and 4
   (check andmap R0? tests)
   (for-each
    (lambda (x)
